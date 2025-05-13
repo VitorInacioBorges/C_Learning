1c6d8c5fc45f0a3ec016c04ad3786a335d88e6b6
Vitor Inacio Borges <vitorinacioborges@gmail.com>
2025-05-13 12:39:15 -0300

    Do While

---
+++ Repetition Structure - Do While/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Lista de Exercicio 10 - Do While
+// Aluno: Vitor Inacio Borges
+// Data: 13/05/2025
+
+int main()
+{
+    /*
+    // Exercicio 1:
+    int soma1_1 = 0, num1_1 = 1, cont1_1 = 0;
+    do {
+        if ((num1_1 % 2) == 0){
+            soma1_1 += num1_1;
+            cont1_1++;
+        }
+        num1_1++;
+    } while (cont1_1 < 50);
+    printf("%d", soma1_1);
+
+
+    // Exercicio 2:
+    int num2_1 = 10;
+    do {
+        printf("%d\n", num2_1);
+        num2_1--;
+    } while (num2_1 > -1);
+    printf("FIM!");
+
+
+    // Exercicio 3:
+    int num3_1, soma3_1, divisor3_1 = 1;
+    printf("Escreva um numero: ");
+    scanf("%d", &num3_1);
+    do {
+        if ((num3_1 % divisor3_1) == 0){
+            soma3_1 += divisor3_1;
+        }
+        divisor3_1++;
+    } while (divisor3_1 < num3_1);
+    printf("%d", soma3_1);
+
+
+    // Exercicio 4:
+    int num4_1 = 1, linha4_1 = 1, cont4_1 = 1, limite4_1;
+    printf("Escreva um numero: ");
+    scanf("%d", &limite4_1);
+    do {
+        if (cont4_1 < linha4_1){
+            printf("%d ", num4_1);
+            cont4_1++;
+            num4_1++;
+        } else {
+            printf("%d\n", num4_1);
+            linha4_1++;
+            num4_1++;
+            cont4_1 = 1;
+        }
+    } while (num4_1 <= limite4_1);
+
+    // Exercicio 5:
+    int numero, dia, mes, ano, quantidade;
+    float preco, total = 0;
+    do {
+        printf("Escreva o numero do pedido (0 para finalizar): ");
+        scanf("%d", &numero);
+        if (numero == 0){
+            break;
+        }
+        printf("Escreva a data (DD/MM/AAAA): ");
+        scanf("%d/%d/%d", &dia, &mes, &ano);
+
+        printf("Escreva a quantidade: ");
+        scanf("%d", &quantidade);
+
+        printf("Escreva o valor em R$: ");
+        scanf("%f", &preco);
+        total += preco * quantidade;
+    } while (1);
+    printf("O valor total fica R$%0.2f", total);
+    */
+
+    // Exercicio 6:
+
+}
